61f9fa128e6a5e23730432e2b70d110efea5ebfa
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2025-12-05 11:30:32 +0900

    update check

--- src/hooks/state/useObjToSingle.ts
+++ src/hooks/state/useObjToSingle.ts
@@ -1,6 +1,6 @@
 import type {KeysWithValue} from "@/types";
 import type {Dispatch, SetStateAction} from "react";
-import {useCallback} from "react";
+import {useCallback, useRef} from "react";
 
 const useObjToSingle = <Type extends object, Target>(
     name: KeysWithValue<Type, Target> | string,
@@ -8,13 +8,14 @@ const useObjToSingle = <Type extends object, Target>(
     set: Dispatch<SetStateAction<Type>>
 ): [Target, Dispatch<SetStateAction<Target>>] => {
     const value: Target = (get as Record<string, Target>)?.[name as string];
+
     const setValue: Dispatch<SetStateAction<Target>> = useCallback((action: SetStateAction<Target>) => {
-        set((state) => ({
-            ...state,
-            [name]: typeof action === "function"
+        set((state) => {
+            const newValue = typeof action === "function"
                 ? (action as (prev: Target) => Target)((state as Record<string, Target>)?.[name as string])
-                : action
-        }));
+                : action;
+            return ({ ...state, [name]: newValue });
+        });
     }, [name, set]);
     return [value, setValue];
 }
